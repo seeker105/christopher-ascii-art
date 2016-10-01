ce8cbbd837ba56198dce074f7ac4d98962033c06
Christopher Soden <karavol27@gmail.com>
2016-09-30 21:15:01 -0600

    initial commit

---
+++ spec/christopher-ascii-art-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ChristopherAsciiArtView from '../lib/christopher-ascii-art-view';
+
+describe('ChristopherAsciiArtView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
